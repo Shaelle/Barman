4ab262a10a0f3513e13134d5e7e60bb71dbd4589
Olga Panisova <shadenoff@gmail.com>
2020-09-02 19:43:45 +0300

    Added basic "push" mechanic.

---
+++ Assets/Scripts/Drink.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drink : MonoBehaviour
+{
+
+    [SerializeField] float speed = 5f;
+
+    bool isMoving = false;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isMoving) transform.Translate(-speed * Time.deltaTime, 0, 0);
+    }
+
+
+    public void Pushing()
+    {
+        isMoving = true;
+    }
+
+    public void Stop()
+    {
+        isMoving = false;
+    }
+
+}
---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class LevelManager : MonoBehaviour
+{
+
+
+    Vector2 cursorPos;
+
+    bool isPushing = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void OnMove(InputValue value)
+    {
+     
+
+        Vector2 newPos = value.Get<Vector2>();
+
+
+        if (newPos.y > cursorPos.y)
+        {
+            RaycastHit hit;
+
+            Ray ray = Camera.main.ScreenPointToRay(newPos);
+            if (Physics.Raycast(ray, out hit))
+            {
+                GameObject hitObject = hit.transform.gameObject;
+
+                Drink drink = hitObject.GetComponent<Drink>();
+
+                if (drink != null && !isPushing)
+                {
+                    drink.Pushing();
+                    //isPushing = true;
+                }
+
+            }
+        }
+
+        cursorPos = newPos;
+
+
+    }
+}
