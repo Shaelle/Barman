04009c20173eea0c6b4741649ec1d27aae3ae57f
Olga Panisova <shadenoff@gmail.com>
2020-10-05 15:45:14 +0300

    Drinks now falling down right after they leaved the table.

---
+++ Assets/Scripts/TableBorder.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TableBorder : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Drink drink = other.GetComponent<Drink>();
+
+        if (drink != null)
+        {
+            drink.SetDirection(0, 0);
+
+              //Debug.Log("drink: " + drink.name);
+        }
+    }
+}
